fcd77fa411488db51914c3339357a1da611a04b7
soapbrain <43698523+mdsoapbrain@users.noreply.github.com>
2022-07-24 12:59:50 +0800

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -4,6 +4,6 @@
 #PBS -l walltime=1:00:00
 #PBS -l nodes=1:ppn=12
 source activate metagenomics-tutorial
-cd /home/hpc/cychen/user/qmi000001/week4/metagenomics-tutorial
+cd /home/hpc/cychen/user/<username>/week4/metagenomics-tutorial
  
 spades.py --careful -o P7741_SPADES_OUT -1 trimmed_reads/P7741_R1.fastq.gz -2 trimmed_reads/P7741_R2.fastq.gz -s trimmed_reads/P7741_S.fastq.gz

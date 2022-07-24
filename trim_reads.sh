2e70a6ec532f76f204ba3e258037a57f3169f851
soapbrain <43698523+mdsoapbrain@users.noreply.github.com>
2022-07-24 12:32:26 +0800

    Update trim_reads.sh

--- trim_reads.sh
+++ trim_reads.sh
@@ -5,7 +5,7 @@
 #PBS -l walltime=1:00:00
 #PBS -l nodes=1:ppn=12
 source activate metagenomics-tutorial
-cd /home/hpc/cychen/user/qmi000001/metagenomics-tutorial/data
+cd /home/hpc/cychen/user/<username>/week4/metagenomics-tutorial/data
 mkdir trimmed_reads
 #trim illumina reads using sickle
 sickle pe -g -f P7741_R1.fastq.gz -r P7741_R2.fastq.gz -t sanger -o trimmed_reads/P7741_R1.fastq.gz -p trimmed_reads/P7741_R2.fastq.gz -s trimmed_reads/P7741_S.fastq.gz -q 20 -l 20
